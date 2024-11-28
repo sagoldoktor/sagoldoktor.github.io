8af0dd5eef64961d0adf84fc0f4ff1d6cd0bee14
sagoldoktor <admin@sagol.xyz>
2024-11-28 15:48:20 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ prompt: userInput }),
+        body: JSON.stringify({ message: userInput }),
       });
 
       const data = await response.json();
