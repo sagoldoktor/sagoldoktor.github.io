f59b835f4f1e7f7a9e3d8ed4f9f89073a6a8fc74
sagoldoktor <admin@sagol.xyz>
2024-11-28 23:30:25 -0800

    Create ChatWindow.js

---
+++ src/ChatWindow.js
@@ -0,0 +1,13 @@
+// src/ChatWindow.js
+import React from 'react';
+import Message from './Message';
+function ChatWindow({ messages }) {
+  return (
+    <div className="chat-window">
+      {messages.map((msg, index) => (
+        <Message key={index} sender={msg.sender} text={msg.text} />
+      ))}
+    </div>
+  );
+}
+export default ChatWindow;
