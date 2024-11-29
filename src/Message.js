175fa99137517ec49104dcf89e69749bc4e82aa7
sagoldoktor <admin@sagol.xyz>
2024-11-29 14:37:30 -0800

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -1,31 +1,16 @@
+// src/Message.js
 import React from 'react';
-import { Box, Typography, Avatar } from '@mui/material';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
-import PersonIcon from '@mui/icons-material/Person';
+import './App.css';
 
 function Message({ sender, text }) {
   const isUser = sender === 'user';
 
   return (
-    <Box
-      display="flex"
-      flexDirection={isUser ? 'row-reverse' : 'row'}
-      alignItems="flex-start"
-      my={1}
-    >
-      <Avatar sx={{ bgcolor: isUser ? 'primary.main' : 'secondary.main' }}>
-        {isUser ? <PersonIcon /> : <LocalHospitalIcon />}
-      </Avatar>
-      <Box
-        mx={1}
-        p={2}
-        borderRadius={2}
-        bgcolor={isUser ? 'primary.light' : 'grey.200'}
-        maxWidth="70%"
-      >
-        <Typography variant="body1">{text}</Typography>
-      </Box>
-    </Box>
+    <div className={`message ${isUser ? 'user' : 'assistant'}`}>
+      <div className="message-content">
+        {text}
+      </div>
+    </div>
   );
 }
 
